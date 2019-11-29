91720e43a6d0bc174347bee103f0ee3a2cf50768
hiaximize <dbarkerit@gmail.com>
2019-11-29 15:48:44 -0500

    made app a MPA

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ app.get('/', (req, resp) => {
     resp.render('index.ejs')
 })
 
+app.get('/aboutus', (req, resp)=>{
+    resp.render('aboutus.ejs')
+})
+
+app.get('/services', (req, resp)=>{
+    resp.render('services.ejs')
+})
+
+app.get('/careers', (req, resp)=>{
+    resp.render('careers.ejs')
+})
+
+app.get('/contactus', (req, resp)=>{
+    resp.render('contactus.ejs')
+})
 
 app.use(express.static('public'))
 
