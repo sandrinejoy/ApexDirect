a2737837c94ca5350d02bbf8ded02573c6b5c5cf
hiaximize <dbarkerit@gmail.com>
2019-12-27 20:52:19 -0500

    added on scroll animation to the index page what we do items

--- public/js/main.js
+++ public/js/main.js
@@ -4,14 +4,27 @@ window.onscroll = function() {myFunction()}
 
 var header = document.getElementById("header")
 
+var mobileHeader = document.getElementById("mobileHeader")
+
 var sticky = header.offsetTop
 
+var mobileSticky = mobileHeader.offsetTop
+
 function myFunction(){
-    if(window.pageYOffset > sticky){
+    if(window.pageYOffset > sticky) {
         header.classList.remove("noSticky")
         header.classList.add("stickyHeader")  
     } else {
         header.classList.remove("stickyHeader")
         header.classList.add("noSticky")
+    }
+
+// ADDED CODE FOR THE MOBILE VIEW STILL MIGHT NEED SOME WORK
+    if(window.pageYOffset > mobileSticky){
+        mobileHeader.classList.remove("noSticky")
+        mobileHeader.classList.add("mobileStickyHeader")
+    } else {
+        mobileHeader.classList.remove("mobileStickyHeader")
+        mobileHeader.classList.add("noSticky")
     }
 }
