3d4d0b85cdcaeb6387a896b94f89e2d9be5b4d6e
hiaximize <dbarkerit@gmail.com>
2020-01-04 20:27:49 -0500

    fixed some incorrect spelling on aboutUs, added pilars profile

--- public/js/careers.js
+++ public/js/careers.js
@@ -5,3 +5,17 @@ document.getElementById("jobAdTitle").addEventListener("click", () => {
     console.log("clicked")
     document.getElementById("jotFormContainer").classList.toggle("display");
 });
+
+var testDiv = document.getElementById("test");
+
+console.log(testDiv)
+
+document.getElementById("myButton").addEventListener("click", ()=>{
+    if(testDiv.classList.contains("hide")){
+        testDiv.classList.remove("hide")
+        testDiv.classList.add("display")
+    } else {
+        testDiv.classList.add("hide")
+        testDiv.classList.remove("display")
+    }
+})
